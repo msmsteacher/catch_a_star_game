90a1252801183aeb06eb8b3715f8d67966dd42fb
msmsteacher <satoyama0410@gmail.com>
2020-11-22 20:21:47 +0900

    Revert "Update Media.py"
    This reverts commit f2819087807e1d27d70269b7e017bf43af9edcf5.

--- Media.py
+++ Media.py
@@ -31,9 +31,8 @@ class Media():
         except FileExistsError:
             pass
 
-    def play_bgm(self, num): 
-        pygame.mixer.quit()       
-        pygame.mixer.init(frequency = 22050, buffer=512)
+    def play_bgm(self, num):        
+        pygame.mixer.init(frequency = 44100)
         pygame.mixer.music.load(self.resource_path("music{0}.mp3").format(num))
         pygame.mixer.music.play(-1)
 
